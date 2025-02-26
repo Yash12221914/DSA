b7253322a8b58be586f55cd3fcf1b406b5c9f481
Yash Raj <yash737raj@gmail.com>
2025-02-26 19:56:06 +0530

    Add files via upload

---
+++ Leetcode/wordLadder1.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<queue>
+#include<unordered_set>
+
+class Ans{
+    public:
+    int ladderLength(string begin,string end,vector<string>& arr){
+        queue<pair<string,int>> q;
+        q.push({begin,1});
+        unordered_set<string> st(arr.begin(),arr.end());
+        st.erase(begin);
+        while(!q.empty()){
+            string word = q.front().first;
+            int step = q.front().second;
+            q.pop();
+            if(word == end) return step;
+            for(int i = 0;i<word.size();i++){
+                char original = word[i];
+                for(char ch = 'a';ch<='z';ch++){
+                    word[i] = ch;
+                    if(st.find(word) != st.end()){
+                        st.erase(word);
+                        q.push({word,step+1});
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return 0;
+    }
+
+};
+int main(){
+    int tc;
+    cin>>tc;
+    while(tc--){
+    int n;
+    cin>>n;
+    vector<string>arr(n);
+    for(int i = 0;i<n;i++) cin>>arr[i];
+    string begin,end;
+    cin>>begin>>end;
+    Ans obj;
+    int answer = obj.ladderLength(begin,end,arr);
+    cout<<answer<<"\n";
+    cout<<"~"<<"\n";
+    }
+    return 0;
+}
