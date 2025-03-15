8fea39ca3b570ea618d13bd58aa7b7dcf6ee9b78
Yash Raj <yash737raj@gmail.com>
2025-03-15 16:46:18 +0530

    Add files via upload

---
+++ Leetcode/array/majorityElement.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,l,r)     for(int i=l; i<r; i++)
+#define int             long long
+#define pb              push_back
+#define vi              vector<int>
+#define vs              vector<string>
+#define vvi             vector<vector<int>>
+#define vvs             vector<vector<string>>
+#define mkp             make_pair<int,int>
+#define umpii           unordered_map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int, vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define in_arr(A,n)     loop(i,0,n) cin>>A[i];
+#define p_arr(A,n)      loop(i,0,n) cout<<A[i];
+#define pln_arr(A,n)    loop(i,0,n) cout<<A[i]<<endl
+#define take_n          int n; cin>>n;
+#define take_arr        int arr[n]; loop(i,0,n) cin>>arr[i];
+#define take_vi         vi(n);loop(i,0,n) cin>>arr[i];
+#define take_vs         vs(n);loop(i,0,n) cin>>arr[i];
+  
+const int mod= 1e9+7;
+const int inf= 1e15;
+ 
+ 
+void solve(){
+    int n;cin>>n;
+    vi arr(n);
+    loop(i,0,n){
+        cin>>arr[i];
+    }
+    //To find the element appears more than n/2,
+    //Approach we can use the map ds but we can do it in orther way also
+    sort(arr.begin(),arr.end());
+    cout<<arr[n/2];
+}
+  
+  
+int32_t main(){
+int t;
+cin>>t;
+while(t--) solve();
+return 0;
+}
