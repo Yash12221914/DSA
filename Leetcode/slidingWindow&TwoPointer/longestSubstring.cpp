79e283952c6d955894205e64dcf37c287bb96f0f
Yash Raj <yash737raj@gmail.com>
2025-03-16 21:09:00 +0530

    Add files via upload

---
+++ Leetcode/slidingWindow&TwoPointer/longestSubstring.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,l,r)     for(int i=l; i<r; i++)
+#define int             long long
+#define pb              push_back
+#define vi              vector<int>
+#define vs              vector<string>
+#define vvi             vector<vector<int>>
+#define vvs             vector<vector<string>>
+#define mkp             make_pair<int,int>
+#define umpii           unordered_map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int, vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define in_arr(A,n)     loop(i,0,n) cin>>A[i];
+#define p_arr(A,n)      loop(i,0,n) cout<<A[i];
+#define pln_arr(A,n)    loop(i,0,n) cout<<A[i]<<endl
+#define take_n          int n; cin>>n;
+#define take_arr        int arr[n]; loop(i,0,n) cin>>arr[i];
+#define take_vi         vi(n);loop(i,0,n) cin>>arr[i];
+#define take_vs         vs(n);loop(i,0,n) cin>>arr[i];
+  
+const int mod= 1e9+7;
+const int inf= 1e15;
+ 
+ 
+void solve(){
+    string s;
+    cin>>s;
+    int n = s.length();
+    int l = 0,r = 0,maxLen = 0;
+    vi hash(256,-1);
+    while (r<n)
+    {
+        int len = 0;
+        if(hash[s[r]] != -1){
+            if(hash[s[r]] >= l){
+                l = hash[s[r]] + 1;
+            }
+        }
+        len = r-l+1;
+        maxLen = max(len,maxLen);
+        hash[s[r]] = r;
+        r++;
+    }
+    cout<<maxLen<<endl;
+    
+}
+  
+  
+int32_t main(){
+int t;
+cin>>t;
+while(t--) solve();
+return 0;
+}
