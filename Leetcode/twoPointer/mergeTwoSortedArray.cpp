47d74370f9e370b904db908f738fe98d5df8e013
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2025-03-17 17:30:30 +0530

    updated github

---
+++ Leetcode/twoPointer/mergeTwoSortedArray.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define loop(i,l,r)     for(int i=l; i<r; i++)
+#define int             long long
+#define pb              push_back
+#define vi              vector<int>
+#define vs              vector<string>
+#define vvi             vector<vector<int>>
+#define vvs             vector<vector<string>>
+#define mkp             make_pair<int,int>
+#define umpii           unordered_map<int,int>
+#define maxheap         priority_queue<int>
+#define minheap         priority_queue<int, vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define zerobits(x)     __builtin_ctzll(x)
+#define in_arr(A,n)     loop(i,0,n) cin>>A[i];
+#define p_arr(A,n)      loop(i,0,n) cout<<A[i];
+#define pln_arr(A,n)    loop(i,0,n) cout<<A[i]<<endl
+#define take_n          int n; cin>>n;
+#define take_arr        int arr[n]; loop(i,0,n) cin>>arr[i];
+#define take_vi         vi(n);loop(i,0,n) cin>>arr[i];
+#define take_vs         vs(n);loop(i,0,n) cin>>arr[i];
+  
+const int mod= 1e9+7;
+const int inf= 1e15;
+ 
+ 
+void solve(){
+    int n; cin >> n;
+    vi arr1(n);
+    loop(i, 0, n) {
+        cin >> arr1[i];
+    }
+
+    int m; cin >> m;
+    vi arr2(m);
+    loop(i, 0, m) {
+        cin >> arr2[i];
+    }
+
+    vi res;
+    int i = 0, j = 0;
+
+    // Merge two sorted arrays
+    while (i < n && j < m) {
+        if (arr1[i] <= arr2[j]) {
+            res.push_back(arr1[i]);
+            i++;
+        } else {
+            res.push_back(arr2[j]);
+            j++;
+        }
+    }
+
+    // Copy remaining elements from arr1
+    while (i < n) {
+        res.push_back(arr1[i]);
+        i++;
+    }
+
+    // Copy remaining elements from arr2
+    while (j < m) {
+        res.push_back(arr2[j]);
+        j++;
+    }
+
+    // Print merged array
+    loop(i, 0, res.size()) {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+}
+  
+  
+int32_t main(){
+int t;
+cin>>t;
+while(t--) solve();
+return 0;
+}
