bf65d2d5baae48c043e0e7ea078d2763383722e8
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2025-04-23 00:09:32 +0530

    update

--- Leetcode/monotonicStack/cpp.cpp
+++ Leetcode/monotonicStack/cpp.cpp
@@ -6,7 +6,10 @@ int main(){
     int m = 3;
 
     vector<vector<int>> arr(n,vector<int> (m));
-    arr = {{1,2,3},{4,5,6},{7,8,9}};
+    arr = {{1,2,3} ,
+            {4,5,6},
+            {7,8,9}
+        };
 
     for(int i = 0;i<arr.size();i++){
         for(int j = i;j<arr[0].size();j++){
